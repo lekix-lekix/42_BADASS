f2f3a71441d0e380e6ac316c6098f81054f099aa
lekix <pouliquen.k@gmail.com>
2026-02-19 16:19:35 +0000

    fix needed

--- P2/config_machines.sh
+++ P2/config_machines.sh
@@ -1,47 +1,41 @@
-#! /usr/bin/sh -x
+#!/bin/bash -x
 
-MACHINES=$(docker ps | awk '{print $NF}' | grep GNS3)
+machines=$(docker ps | awk '{print $NF}' | grep GNS3)
 
 ip_host_1="30.1.1.1/24"
 ip_host_2="30.1.1.2/24"
 
-ip_router1="10.1.1.1/24"
-ip_router2="10.1.1.2/24"
-vxlan_ip1="20.1.1.1/24"
-vxlan_ip2="20.1.1.2/24"
+ip_router_1="10.1.1.1"
+ip_router_2="10.1.1.2"
+vxlan_ip_1="20.1.1.1"
+vxlan_ip_2="20.1.1.2"
 
-router_ip=""
-vxlan_ip=""
-
-router1_config=$(cat << 'EOF'
-ip link add br0 type bridge
-ip link set dev br0 up
-ip addr add ${ip} dev eth1
-/sbin/ip link add name vxlan10 type vxlan id 10 dev eth0 remote 10.1.1.2 local 10.1.1.1 dstport 4789
-ip addr add ${vxlan_ip} dev vxlan10
-brctl addif br0 eth1
-brctl addif br0 vxlan10
-ip link set dev vxlan10 up
-EOF
-)
-
-for i in {1..2}
+for i in 1 2
 do
-    name=$(echo "$MACHINES" | awk -v col="$i" 'NR==1{print $col}')
-    echo "NAME = $name"
-    if echo $name | grep -q "host-1"; then
-#	echo "docker exec $name ip addr add ${ip_host_1} dev eth1"
-	docker exec $name ip addr add ${ip_host_1} dev eth1
-    elif echo $name | grep -q "host-2"; then
-	docker exec $name ip link add ${ip_host_2} dev eth1
-    elif echo $name | grep -q "router"; then
-	if echo $name | grep -q "router-1"; then
-	    router_ip=$ip_router_1
-	    vxlan_ip=$vxlan_ip_1
-        else
-	    router_ip=$ip_router2
-	    vxlan_ip=$vxlan_ip2
-	fi
-	docker exec $name $router1_config
+    if echo $machines | grep -q "host-"; then
+	name=$(echo "$machines" | grep "host-$i")
+        ip_machine="ip_host_${i}"
+	docker exec $name ip addr add ${!ip_machine} dev eth1
+    fi
+done
+
+for i in 1 2
+do
+    if echo $machines | grep -q "router-"; then
+	name=$(echo "$machines" | grep "router-$i")
+	router_ip="ip_router_${i}"
+	if [ $i -eq 1 ]; then
+	    other_router_ip=${!ip_router_2}
+	else
+	    other_router_ip=${!ip_router_1}
+	vxlan_ip="vxlan_ip_${i}"
+	docker exec $name ip link add br0 type bridge
+	docker exec $name ip link set dev br0 up
+	docker exec $name ip addr add ${!router_ip} dev eth0
+	docker exec $name /sbin/ip link add name vxlan10 type vxlan id 10 dev eth0 remote 10.1.1.1 local 10.1.1.2 dstport 4789
+	docker exec $name ip addr add ${!vxlan_ip} dev vxlan10
+	docker exec $name brctl addif br0 eth1
+	docker exec $name brctl addif br0 vxlan10
+	docker exec $name ip link set dev vxlan10 up
     fi
 done
